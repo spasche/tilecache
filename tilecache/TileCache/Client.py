35b80d52b5e8907ac2279f2c28fdb0d85b09733f
Frederic Junod <frederic.junod@camptocamp.com>
2009-07-03 08:37:32 +0200

    move the seed function from client to seed

--- tilecache/TileCache/Client.py
+++ tilecache/TileCache/Client.py
@@ -2,7 +2,7 @@
 
 # BSD Licensed, Copyright (c) 2006-2008 MetaCarta, Inc.
 
-import sys, urllib, urllib2, time, os, math
+import sys, urllib, urllib2
 import httplib
 
 # setting this to True will exchange more useful error messages
@@ -68,51 +68,3 @@ class WMS (object):
 
     def setBBox (self, box):
         self.params["bbox"] = ",".join(map(str, box))
-
-def seed (svc, layer, levels = (0, 5), bbox = None, padding = 0, force = False, reverse = False ):
-    from Layer import Tile
-    try:
-        padding = int(padding)
-    except:
-        raise Exception('Your padding parameter is %s, but should be an integer' % padding)
-
-    if not bbox: bbox = layer.bbox
-
-    start = time.time()
-    total = 0
-    
-    for z in range(*levels):
-        bottomleft = layer.getClosestCell(z, bbox[0:2])
-        topright   = layer.getClosestCell(z, bbox[2:4])
-        # Why Are we printing to sys.stderr??? It's not an error.
-        # This causes a termination if run from cron or in background if shell is terminated
-        #print >>sys.stderr, "###### %s, %s" % (bottomleft, topright)
-        print "###### %s, %s" % (bottomleft, topright)
-        zcount = 0 
-        metaSize = layer.getMetaSize(z)
-        ztiles = int(math.ceil(float(topright[1] - bottomleft[1]) / metaSize[0]) * math.ceil(float(topright[0] - bottomleft[0]) / metaSize[1]))
-        if reverse:
-            startX = topright[0] + metaSize[0] + (1 * padding)
-            endX = bottomleft[0] - (1 * padding)
-            stepX = -metaSize[0]
-            startY = topright[1] + metaSize[1] + (1 * padding)
-            endY = bottomleft[1] - (1 * padding)
-            stepY = -metaSize[1]
-        else:
-            startX = bottomleft[0] - (1 * padding)
-            endX = topright[0] + metaSize[0] + (1 * padding)
-            stepX = metaSize[0]
-            startY = bottomleft[1] - (1 * padding)
-            endY = topright[1] + metaSize[1] + (1 * padding)
-            stepY = metaSize[1]
-        for y in range(startY, endY, stepY):
-            for x in range(startX, endX, stepX):
-                tileStart = time.time()
-                tile = Tile(layer,x,y,z)
-                bounds = tile.bounds()
-                svc.renderTile(tile,force=force)
-                total += 1
-                zcount += 1
-                box = "(%.4f %.4f %.4f %.4f)" % bounds
-                print "[%s] %02d (%06d, %06d) = %s [%.4fs : %.3f/s] %s/%s" \
-                     % (layer.name, z,x,y, box, time.time() - tileStart, total / (time.time() - start + .0001), zcount, ztiles)
--- tilecache/TileCache/Seed/Seed.py
+++ tilecache/TileCache/Seed/Seed.py
@@ -3,6 +3,12 @@ import time
 from MapScript import getLayersByName, tiles
 
 def seed(service, layer, skip_empty=True, levels=None, bbox=None, padding=0, force=False, reverse=False):
+    if not levels:
+        levels = (0, len(layer.resolutions))
+
+    if not bbox:
+        bbox = layer.bbox
+
     if skip_empty and hasattr(layer, 'mapfile'):
         # a mapserver layer
         mapObj = mapscript.mapObj(layer.mapfile)
@@ -19,12 +25,33 @@ def seed(service, layer, skip_empty=True, levels=None, bbox=None, padding=0, for
         skip_empty = False
 
     if skip_empty:
+        # metaSize, reverse, padding not managed
         for x, y, z in tiles(layersObj, layer):
             start = time.time()
             tile = Tile(layer, x, y, z)
             service.renderTile(tile, force=force)
-            print "['%s'] (x: %04d, y: %04d, z: %04d) in %4fs"%(layer.name, x, y, z, time.time() - start))
-            
+            print "['%s'] (x: %04d, y: %04d, z: %04d) in %4fs"%(layer.name, x, y, z, time.time() - start)
+
     else:
-        # call the 'old' function
-        pass
+        for z in range(*levels):
+            minx, miny = layer.getClosestCell(z, bbox[0:2])
+            maxx, maxy = layer.getClosestCell(z, bbox[2:4])
+            metax, metay = layer.getMetaSize(z)
+
+            startx = minx - (1 * padding)
+            endx = maxx + metax + (1 * padding)
+            starty = miny - (1 * padding)
+            endy = maxy + metay + (1 * padding)
+
+            if reverse:
+                startx, endx = endx, startx
+                starty, endy = endy, starty
+                metax = -metax
+                metay = -metay
+
+            for y in range(starty, endy, metay):
+                for x in range(startx, endx, metax):
+                    start = time.time()
+                    tile = Tile(layer, x, y, z)
+                    service.renderTile(tile, force=force)
+                    print "['%s'] (x: %04d, y: %04d, z: %04d) in %4fs"%(layer.name, x, y, z, time.time() - start)
