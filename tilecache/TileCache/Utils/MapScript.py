3febab1e4bd0e28a2afbe414a3e56ee828732b65
Frederic Junod <frederic.junod@camptocamp.com>
2009-07-10 13:47:16 +0200

    shapes() function for a raster layer

--- tilecache/TileCache/Utils/MapScript.py
+++ tilecache/TileCache/Utils/MapScript.py
@@ -6,12 +6,24 @@ def valid_extent(rectObj):
     return rectObj.minx != -1 and rectObj.miny != -1 and \
            rectObj.maxx != -1 and rectObj.maxy != -1
 
+# def shapes(layerObj, extent=None):
+#     # FIXME: extent
+#     tileindex = os.path.join(layerObj.map.shapepath, layerObj.tileindex + ".shp")
+#     ds = ogr.Open(tileindex)
+#     layer = ds.GetLayerByIndex(0) # ?
+#     tile = layer.GetNextFeature()
+#     while tile:
+#         miny, maxy, minx, maxx = tile.GetGeometryRef().GetEnvelope()
+#         yield mapscript.rectObj(minx, miny, maxx, maxy).toPolygon()
+#         tile = layer.GetNextFeature()
+#     ds.Destroy()
+
 def shapes(layerObj, extent=None):
     """ return all the shapes from 'layerObj' inside 'extent' """
     # convert the extent to a rectObj if needed
     if extent is not None and not isinstance(extent, mapscript.rectObj):
         extent = mapscript.rectObj(*extent)
-    
+
     if extent is None or not valid_extent(extent):
         if valid_extent(layerObj.extent):
             extent = layerObj.extent
