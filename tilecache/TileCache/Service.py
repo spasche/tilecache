97f7fb766ffe51e4c068387fe4e2d115db93d7e4
Frederic Junod <frederic.junod@camptocamp.com>
2009-09-09 09:21:43 +0200

    move the ImageMerger classes into TileCache.Utils

--- tilecache/TileCache/Service.py
+++ tilecache/TileCache/Service.py
@@ -8,12 +8,9 @@ import time
 import os
 import traceback
 import ConfigParser
-try:
-    import cStringIO as StringIO
-except ImportError:
-    import StringIO
 
 from TileCache.Cache import Cache
+from TileCache.Utils.ImageMerger import ImageMergeMerger, PILMerger
 import Layer
 
 class TileCacheException(Exception):
@@ -49,78 +46,6 @@ def import_module(name):
     for comp in components[1:]:
         mod = getattr(mod, comp)
     return mod
-
-class ImageMerger:
-    def __init__ (self, service):
-        self.service = service
-
-    def preferred(self, tiles, params):
-        """Override this method and return False is this module may not be
-           the best option for the given parameters"""
-        return True
-
-class ImageMergeMerger(ImageMerger):
-    @staticmethod
-    def available():
-        if "TC_NO_IMAGE_MERGE" in os.environ:
-            return False
-        try:
-            import image_merge
-        except ImportError:
-            return False
-        return True
-
-    def preferred(self, tiles, params):
-        # Empirical measurements have determined than PIL is faster when merging
-        # a small number of layers. So this module shouldn't be the preferred
-        # one in that case.
-        # This number has been calculed by on a 64-bit Debian system.
-        # Note that results are different when running on a 32-bit system.
-        IMAGE_MERGE_TILES_THRESHOLD = 5
-
-        return len(tiles) > IMAGE_MERGE_TILES_THRESHOLD
-
-    def merge(self, tiles, params):
-        import image_merge
-
-        images = []
-        for t in tiles:
-            (format, data) = self.service.renderTile(t, params.has_key('FORCE'))
-            if data is None:
-                continue
-            images.append(data)
-
-        return (format, image_merge.merge(*images))
-                    
-class PILMerger(ImageMerger):
-    @staticmethod
-    def available():
-        try:
-            import PIL.Image
-        except ImportError:
-            return False
-        return True
-                    
-    def merge(self, tiles, params):
-        import PIL.Image as Image
-        result = None
-        for t in tiles:
-            (format, data) = self.service.renderTile(t, params.has_key('FORCE'))
-            if data is None:
-                continue
-            image = Image.open(StringIO.StringIO(data))
-            if not result:
-                result = image
-            else:
-                result.paste(image, None, image)
-
-        if result is None:
-            return (format, None)
-        else:
-            buffer = StringIO.StringIO()
-            result.save(buffer, result.format)
-            buffer.seek(0)
-            return (format, buffer.read())
 
 class Service (object):
     __slots__ = ("layers", "cache", "metadata", "tilecache_options", "config", "files", "image_mergers")
---
+++ tilecache/TileCache/Utils/ImageMerger.py
@@ -0,0 +1,78 @@
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
+
+__all__ = ['ImageMergeMerger', 'PILMerger']
+
+class ImageMerger:
+    def __init__ (self, service):
+        self.service = service
+
+    def preferred(self, tiles, params):
+        """Override this method and return False is this module may not be
+           the best option for the given parameters"""
+        return True
+
+class ImageMergeMerger(ImageMerger):
+    @staticmethod
+    def available():
+        if "TC_NO_IMAGE_MERGE" in os.environ:
+            return False
+        try:
+            import image_merge
+        except ImportError:
+            return False
+        return True
+
+    def preferred(self, tiles, params):
+        # Empirical measurements have determined than PIL is faster when merging
+        # a small number of layers. So this module shouldn't be the preferred
+        # one in that case.
+        # This number has been calculed by on a 64-bit Debian system.
+        # Note that results are different when running on a 32-bit system.
+        IMAGE_MERGE_TILES_THRESHOLD = 5
+
+        return len(tiles) > IMAGE_MERGE_TILES_THRESHOLD
+
+    def merge(self, tiles, params):
+        import image_merge
+
+        images = []
+        for t in tiles:
+            (format, data) = self.service.renderTile(t, params.has_key('FORCE'))
+            if data is None:
+                continue
+            images.append(data)
+
+        return (format, image_merge.merge(*images))
+
+class PILMerger(ImageMerger):
+    @staticmethod
+    def available():
+        try:
+            import PIL.Image
+        except ImportError:
+            return False
+        return True
+
+    def merge(self, tiles, params):
+        import PIL.Image as Image
+        result = None
+        for t in tiles:
+            (format, data) = self.service.renderTile(t, params.has_key('FORCE'))
+            if data is None:
+                continue
+            image = Image.open(StringIO.StringIO(data))
+            if not result:
+                result = image
+            else:
+                result.paste(image, None, image)
+
+        if result is None:
+            return (format, None)
+        else:
+            buffer = StringIO.StringIO()
+            result.save(buffer, result.format)
+            buffer.seek(0)
+            return (format, buffer.read())
