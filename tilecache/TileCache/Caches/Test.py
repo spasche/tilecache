2003b7a64cf66045f31dd062810091c16c786ce6
Frederic Junod <frederic.junod@camptocamp.com>
2009-10-30 10:31:57 +0100

    the Test cache is never readonly

--- tilecache/TileCache/Caches/Test.py
+++ tilecache/TileCache/Caches/Test.py
@@ -7,6 +7,11 @@ class Test(Cache):
     TileCache should not cache data. In general, using this
     with metatiles is very slow, and not recommended.
     """
+    def __init__ (self, **kwargs):
+        Cache.__init__(self, **kwargs)
+        # the Test cache is never readonly
+        self.readonly = False
+    
     def get(self, tile):
         return None
     
